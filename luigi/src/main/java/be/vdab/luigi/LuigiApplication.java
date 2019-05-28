73be8733b8e671123c3144886575d274beea5866
withouckvincent <withouck.vincent@gmail.com>
2019-05-28 11:55:56 +0200

    hoofdstuk 38

--- luigi/src/main/java/be/vdab/luigi/LuigiApplication.java
+++ luigi/src/main/java/be/vdab/luigi/LuigiApplication.java
@@ -3,15 +3,25 @@ package be.vdab.luigi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 // hoofdstuk 37.1
 //@ImportResource("restclients.xml")
 
 @SpringBootApplication
-public class LuigiApplication {
+public class LuigiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LuigiApplication.class, args);
 	}
 
+	
+	@Override
+	protected SpringApplicationBuilder configure(
+	SpringApplicationBuilder application) {
+	return application.sources(LuigiApplication.class);
+	}
+
+	
 }
